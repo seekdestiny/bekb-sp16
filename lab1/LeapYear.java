7967bd0db7bcff1c12d1c5fe6e033d6d6e44efe6
seekdestiny <jifeiqian@gmail.com>
2016-12-01 21:19:25 -0800

    Leap Year Test

---
+++ lab1/LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2001;
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
