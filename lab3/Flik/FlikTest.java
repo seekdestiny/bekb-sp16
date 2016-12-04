f7a7e303561fd485884e64fc1ffa562723fe7e93
seekdestiny <jifeiqian@gmail.com>
2016-12-03 17:23:23 -0800

    complete lab3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void testIsSameNumber() {
+        assertEquals(true, Flik.isSameNumber(127, 127));
+        assertEquals(true, Flik.isSameNumber(128, 128));
+    }
+}
